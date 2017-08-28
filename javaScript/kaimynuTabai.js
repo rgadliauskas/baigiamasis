2bad786c7303485c63fce255bc556261e4d0d287
rgadliauskas <r.gadliauskas@gmail.com>
2017-08-28 19:47:28 +0300

    Padaryti kontaktai, patvarkyta galerija, uzdetas fonas

--- javaScript/kaimynuTabai.js
+++ javaScript/kaimynuTabai.js
@@ -1,13 +1,23 @@
 $(function () {
 
+    var url = window.location.href;
+    var hash = url.substring(url.indexOf("#") + 1);
+
 
     $('.tab').click(function () {
-        var tabId = $(this).attr('href');
+        var tabId = '.'+$(this).attr('tabas');
 
         $(tabId).show().siblings().hide();
 
         $(this).addClass('active').siblings().removeClass('active');
-    });
 
+        // e.preventDefault();
+    });
     $('[data-default]').click();
+
+    if(hash){
+        $('[tabas="' + hash + '"]').click();
+    }
+
 });
+
